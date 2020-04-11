22f2e3e81f3da0423a44df976b1a992b5a828fc2
Xavier Antoviaque <xavier@antoviaque.org>
2020-04-11 12:12:39 -0400

    Mock create room via API

--- schedule/tests.py
+++ schedule/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
-# Create your tests here.
+SAMPLE_CALENDLY_WEBHOOK_POST_DATA = b'{"event":"invitee.created","payload":{"event_type":{"uuid":"BHHFEDKAVI6AJAHA","kind":"One-on-One","slug":"30min","name":"30 Minute Meeting","duration":30,"owner":{"type":"users","uuid":"f47dc814f55fc5a19227c01c6b8d2307"}},"event":{"uuid":"FHPWD2PTRYQENSFB","assigned_to":["Xavier Antoviaque"],"extended_assigned_to":[{"name":"Xavier Antoviaque","email":"xavier@opencraft.com","primary":true}],"start_time":"2020-04-30T17:30:00Z","start_time_pretty":"05:30pm - Thursday, April 30, 2020","invitee_start_time":"2020-04-30T13:30:00-04:00","invitee_start_time_pretty":"01:30pm - Thursday, April 30, 2020","end_time":"2020-04-30T18:00:00Z","end_time_pretty":"06:00pm - Thursday, April 30, 2020","invitee_end_time":"2020-04-30T14:00:00-04:00","invitee_end_time_pretty":"02:00pm - Thursday, April 30, 2020","created_at":"2020-04-09T18:51:41Z","location":"https://zoom.us/j/212890698","canceled":false,"canceler_name":null,"cancel_reason":null,"canceled_at":null},"invitee":{"uuid":"FDLQS5YPKWXW4COP","first_name":"Bob","last_name":"Test","name":"Bob Test","email":"xavier+test@antoviaque.org","text_reminder_number":null,"timezone":"America/New_York","created_at":"2020-04-09T18:51:41Z","is_reschedule":false,"payments":[],"canceled":false,"canceler_name":null,"cancel_reason":null,"canceled_at":null},"questions_and_answers":[{"question":"Topic/agenda","answer":"Test"}],"questions_and_responses":{"1_question":"Topic/agenda","1_response":"Test"},"tracking":{"utm_campaign":null,"utm_source":null,"utm_medium":null,"utm_content":null,"utm_term":null,"salesforce_uuid":null},"old_event":null,"old_invitee":null,"new_event":null,"new_invitee":null},"time":"2020-04-09T18:51:43Z"}'
+
+class CalendlyTests(TestCase):
+
+    def test_receive_webhook(self):
+        c = Client()
+        response = c.generic('POST', '/schedule/calendly/webhook',
+                             data=SAMPLE_CALENDLY_WEBHOOK_POST_DATA)
+        self.assertEqual(response.status_code, 200)
--- schedule/views.py
+++ schedule/views.py
@@ -1,7 +1,34 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+import json
+import urllib
+from pprint import pprint
+
+def create_bbb_meeting(event_uuid, event_name, event_start_time, event_end_time):
+    urllib.urlencode({
+        'meetingID': event_uuid,
+        'name': event_name,
+        #'welcome': 'Welcome!',
+        'logoutURL': 'https://opencraft.com/',
+        'record': 'true',
+        'autoStartRecording': 'true',
+        #'logo': 'https://',
+        #'bannerText': '',
+        #'bannerColor': '',
+        #'copyright': '',
+        #'allowModsToUnmuteUsers': 'true',
+    })
+
 @csrf_exempt
 def calendly_webhook(request):
-    print(request.body)
+    event_dict = json.loads(request.body)
+    event_start_time = event_dict['payload']['event']['start_time']
+    event_end_time = event_dict['payload']['event']['end_time']
+    event_uuid = event_dict['payload']['event']['uuid']
+    event_invitee_name = event_dict['payload']['invitee']['name']
+    event_host_name = event_dict['payload']['event']['assigned_to'][0]
+    event_name = '%s and %s'.values(event_invitee_name, event_host_name)
+
+    create_bbb_meeting(event_uuid, event_name, event_start_time, event_end_time)
     return HttpResponse("OK")
