7bd43c0d1948c94985a9358c4412b9a8d02c641d
Xavier Antoviaque <xavier@antoviaque.org>
2020-04-11 15:40:12 -0400

    Create BBB room for any new calendly invitee & add env settings

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Meet
 
 Online meetings organization helper
+
+## Install
+
+Create `.env` with the variables from `meet/settings.py` that needs to be customized.
+(TODO: Improve installation instructions)
--- schedule/views.py
+++ schedule/views.py
@@ -1,24 +1,64 @@
+from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 import json
+import mechanize
 import urllib
+
+from bs4 import BeautifulSoup
 from pprint import pprint
 
-def create_bbb_meeting(event_uuid, event_name, event_start_time, event_end_time):
-    urllib.urlencode({
-        'meetingID': event_uuid,
-        'name': event_name,
-        #'welcome': 'Welcome!',
-        'logoutURL': 'https://opencraft.com/',
-        'record': 'true',
-        'autoStartRecording': 'true',
-        #'logo': 'https://',
-        #'bannerText': '',
-        #'bannerColor': '',
-        #'copyright': '',
-        #'allowModsToUnmuteUsers': 'true',
-    })
+
+
+def bbb_login(br):
+    def is_login_form(form):
+        return "action" in form.attrs and 'login' in form.attrs['action']
+
+    br.open(settings.BBB_ROOT_URL + '/b/signin')
+    br.select_form(predicate=is_login_form)
+    br.form['session[email]'] = settings.BBB_USERNAME
+    br.form['session[password]'] = settings.BBB_PASSWORD
+    response = br.submit()
+    assert 'Sign out' in response.read().decode('utf-8')
+
+def bbb_get_room(br, room_name):
+    response = br.response()
+    soup = BeautifulSoup(response.read().decode('utf-8'), 'html.parser')
+    for tag in soup.find_all(id='room-name-text'):
+        if tag.contents[0] == room_name:
+            link = tag.find_parents('a')[0]
+            return BBB_ROOT_URL + link['href']
+
+def bbb_create_room(br, room_name):
+    def is_room_create_form(form):
+        try:
+            form.find_control("room[name]")
+        except mechanize._form_controls.ControlNotFoundError:
+            return False
+        else:
+            return True
+
+    br.select_form(predicate=is_room_create_form)
+    br.form['room[name]'] = room_name
+    room_anyone_can_start = br.form.find_control('room[anyone_can_start]', nr=0) # Uses duplicate hidden field
+    room_anyone_can_start.readonly = False
+    room_anyone_can_start.value = '1'
+    response = br.submit()
+    assert room_name in response.read().decode('utf-8')
+
+    return response.geturl()
+
+def get_bbb_room_url(room_name):
+    br = mechanize.Browser()
+    br.set_handle_robots(False)
+
+    bbb_login(br)
+    room_url = bbb_get_room(br, room_name)
+    if not room_url:
+        room_url = bbb_create_room(br, room_name)
+
+    return room_url
 
 @csrf_exempt
 def calendly_webhook(request):
@@ -27,8 +67,10 @@ def calendly_webhook(request):
     event_end_time = event_dict['payload']['event']['end_time']
     event_uuid = event_dict['payload']['event']['uuid']
     event_invitee_name = event_dict['payload']['invitee']['name']
+    event_invitee_email = event_dict['payload']['invitee']['email']
     event_host_name = event_dict['payload']['event']['assigned_to'][0]
-    event_name = '%s and %s'.values(event_invitee_name, event_host_name)
+    event_name = '{} and {}'.format(event_invitee_name, event_host_name)
+    room_name = '{} ({})'.format(event_invitee_name, event_invitee_email)
 
-    create_bbb_meeting(event_uuid, event_name, event_start_time, event_end_time)
+    print(get_bbb_room_url(room_name))
     return HttpResponse("OK")
